d4cb924b52905f7c9555bf7893a8f3fb52126a9c
Halffd <guhalfeld@hotmail.com>
2024-03-31 16:24:03 -0300

    autostart

---
+++ src/App.vue
@@ -0,0 +1,187 @@
+<template>
+  <v-app>
+    <SystemBar v-if="is_electron()"></SystemBar>
+    <router-view name="Header"></router-view>    
+    <v-main style="--v-layout-left: 0px; --v-layout-right: 0px; --v-layout-top: 0px; --v-layout-bottom: 60px; margin-bottom: -60px;">
+      <router-view :footer="footer" class="fullscreen-router-view"></router-view>
+    </v-main>
+    <!-- Hide Footer Button -->
+    <v-btn v-if="!footer" class="corner-button" color="primary" icon @click="toggleFooter">
+    <v-icon>{{ footer ? 'mdi-chevron-down' : 'mdi-chevron-up' }}</v-icon>
+  </v-btn>
+
+    <router-view name="Footer" @toggle-footer-visibility="toggleFooter" ></router-view>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { useAppearanceStore } from  './stores/appearance'
+import { useWordReplaceStore } from  './stores/word_replace'
+import { useSettingsStore } from  './stores/settings'
+import { useSpeechStore } from  './stores/speech'
+import { useTranslationStore } from './stores/translation'
+import { useConnectionStore } from  './stores/connections'
+import { useOSCStore } from  './stores/osc'
+
+import is_electron from './helpers/is_electron'
+
+import SystemBar from './components/appbars/SystemBar.vue'
+
+declare const window: any
+
+export default {
+  name: 'App',
+  components: {
+    SystemBar
+  },
+  setup() {
+    const appearanceStore = useAppearanceStore()
+    const speechStore = useSpeechStore()
+    const wordReplaceStore = useWordReplaceStore()
+    const translationStore = useTranslationStore()
+    const settingsStore = useSettingsStore()
+    const connectionStore = useConnectionStore()
+    const oscStore = useOSCStore()
+
+    appearanceStore.$subscribe((_, state) => {
+        localStorage.setItem('appearance', JSON.stringify(state))
+    })
+    speechStore.$subscribe((_, state) => {
+        localStorage.setItem('speech', JSON.stringify(state))
+    })
+    settingsStore.$subscribe((_, state) => {
+        localStorage.setItem('settings', JSON.stringify(state))
+    })
+    wordReplaceStore.$subscribe((_, state) => {
+        localStorage.setItem('word_replace', JSON.stringify(state))
+    })
+    translationStore.$subscribe((_, state) => {
+        localStorage.setItem('translation', JSON.stringify(state))
+    })
+    connectionStore.$subscribe((_, state) => {
+        localStorage.setItem('connections', JSON.stringify(state))
+    })
+    oscStore.$subscribe((_, state) => {
+        localStorage.setItem('osc', JSON.stringify(state))
+    })
+
+
+    appearanceStore.$patch(JSON.parse(localStorage.getItem('appearance') || '{}'))
+    speechStore.$patch(JSON.parse(localStorage.getItem('speech') || '{}'))
+    settingsStore.$patch(JSON.parse(localStorage.getItem('settings') || '{}'))
+    wordReplaceStore.$patch(JSON.parse(localStorage.getItem('word_replace') || '{}'))
+    translationStore.$patch(JSON.parse(localStorage.getItem('translation') || '{}'))
+    connectionStore.$patch(JSON.parse(localStorage.getItem('connections') || '{}'))
+    oscStore.$patch(JSON.parse(localStorage.getItem('osc') || '{}'))
+
+    return {
+      settingsStore,
+      connectionStore,
+      is_electron,
+    }
+  },
+  data() {
+    return {
+      footer: true
+    };
+  },
+  unmounted() {
+    if (this.is_electron()) window.ipcRenderer.send("close-ws")
+  },
+  mounted() {
+    if (this.is_electron() && this.connectionStore.ws.enabled) 
+      window.ipcRenderer.send("start-ws", this.connectionStore.ws.port)
+
+    this.$i18n.locale = this.settingsStore.language
+    this.settingsStore.$subscribe((language, state) => {
+        this.$i18n.locale = this.settingsStore.language
+    })
+  },
+  methods: {
+    toggleFooter() {
+      this.footer = !this.footer;
+      let el:  HTMLElement | null = document.querySelector("#log-list")
+      let em: HTMLElement | null = document.querySelector(".v-main")
+      let ef: HTMLElement | null = document.querySelector("#app > div > div > footer")
+      let et: HTMLElement | null = document.querySelector(".v-system-bar.systembar")
+      if(et){
+        et.style.display = et.style.display == 'none' ? 'block' : 'none'
+      }
+      if(el && em && ef){
+          if(ef.style.height  != '0px'){
+              ef.style.height = '0'
+              ef.style.padding = '0'
+              em.style.setProperty('margin-bottom', '0');
+              em.style.setProperty('--v-layout-bottom', '0');
+             // el.style.setProperty('--933e9cdf-outer_size', '0');
+          } else {
+              em.style.setProperty('--v-layout-bottom', '60px');
+              //el.style.setProperty('--933e9cdf-outer_size', '55px');
+              ef.style.height = '60px'
+              ef.style.padding = '8px'
+            }
+            //el.scrollTop = el.scrollHeight
+      }
+    },
+  }
+}
+</script>
+
+<style>
+.pointer {
+  cursor: pointer;
+}
+
+::-webkit-scrollbar {
+  width: 8px;
+}
+
+::-webkit-scrollbar-track {
+    background-color: transparent; /* rgb(var(--v-theme-primary)); */
+}
+
+::-webkit-scrollbar-thumb {
+  background: #4e4e4e; /* #a9a4e5 */
+  border-radius: 10px;
+}
+
+/* blink keyframe */
+.text-glow {
+  color: rgba(var(--v-theme-secondary))
+}
+.blink {
+  animation: blinker 1s cubic-bezier(.5, 0, 1, 1) infinite alternate;  
+}
+@keyframes blinker {  
+  from { opacity: 1; }
+  to { opacity: 0; }
+}
+
+#corner-element {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 20px;
+  height: 20px;
+  background-color: red;
+  padding: 0;
+  margin: 0;
+}
+
+.fullscreen-router-view {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+
+<style>
+.corner-button {
+  position: fixed;
+  top: 0; /* Adjust the top value to your desired position */
+  left: 0; /* Adjust the right value to your desired position */
+  transform: scale(0.5);
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,231 @@
+<template>
+  <v-card id="log-list" v-resize="onResize" class="fill-height pa-4 overflow-auto log-list" :color="appearanceStore.ui.color" :height="height - 55" tile>
+    <div>
+      <a
+        v-for="log in logs"
+        :class="{ 'fade-out': log.hide, 'final-text': log.isFinal && log.isTranslationFinal, 'interim-text': !log.isFinal || (!log.isTranslationFinal && log.translate) }"
+      >
+        <a v-if="log.hide !== 2">{{ (translationStore.enabled && (log.translation || !translationStore.show_original)) ? log.translation : log.transcript }}&nbsp;&nbsp;</a>
+        <v-expand-transition v-show="log.pause">
+          <div>
+            <v-col class="pa-0" />
+          </div>
+        </v-expand-transition>
+      </a>
+    </div>
+
+    <WelcomeOverlay :overlay="overlay_main" :page="overlay_page" />
+  </v-card>
+</template>
+
+<script lang="ts">
+// import {ipcRenderer} from "electron"
+import { useDisplay } from 'vuetify'
+
+import is_electron from '@/helpers/is_electron'
+
+import WelcomeOverlay from '@/components/overlays/WelcomeOverlay.vue'
+
+import { useSettingsStore } from '@/stores/settings'
+import { useAppearanceStore } from '@/stores/appearance'
+import { useLogStore } from '@/stores/logs'
+import { useTranslationStore } from '@/stores/translation'
+
+declare const window: any
+
+export default {
+  name: 'Home',
+  components: {
+    WelcomeOverlay,
+  },
+  
+  props: {
+    isElectron: {
+      type: Boolean,
+      required: false
+    },
+    footer: {
+      type: Boolean,
+      required: true
+    }
+  },
+  setup() {
+    const { height } = useDisplay()
+
+    const settingsStore = useSettingsStore()
+    const appearanceStore = useAppearanceStore()
+    const logStore = useLogStore()
+    const translationStore = useTranslationStore()
+
+    const font_size = `${appearanceStore.text.font_size}px`
+    const fade_time = `${appearanceStore.text.fade_time}s`
+    const text_color = appearanceStore.text.color
+    const interim_color = appearanceStore.text.interim_color
+
+    const font_name = appearanceStore.text.font.name
+    const font_subtype = appearanceStore.text.font.sub_type
+
+    return {
+      settingsStore,
+      appearanceStore,
+      logs: logStore.logs,
+      translationStore,
+      font_size,
+      fade_time,
+      text_color,
+      interim_color,
+      font_name,
+      font_subtype,
+      height,
+    }
+  },
+  data() {
+    return {
+      // oscClient: client,
+      overlay_main: false,
+      overlay_page: 0,
+
+      ws: null as any,
+
+      listening: false,
+      listening_error: false,
+      talking: false,
+
+      loadingWebsocket: false,
+      broadcasting: false,
+
+      input_text: '',
+
+      snackbar: false,
+      snackbar_color: 'error',
+      snackbar_icon: '',
+      snackbar_desc: '',
+
+      error_snackbar: false,
+      error_message: '',
+
+      windowSize: {
+        x: 0,
+        y: 0,
+      },
+    }
+  },
+  computed: {
+    outer_size() {
+      return this.footer ? (is_electron() ? '90px' : '55px') : '0px'
+    },
+  },
+  mounted() {
+    this.overlay_main = this.settingsStore.welcome
+    this.onResize()
+  },
+  methods: {
+    onResize() {
+      this.windowSize = { x: window.innerWidth, y: window.innerHeight }
+    },
+  },
+}
+</script>
+
+<style>
+html {
+  overflow-y: hidden;
+}
+
+.log-list {
+  display: flex;
+  flex-direction: column-reverse;
+  font-family: v-bind(font_name);
+  font-style: v-bind('font_subtype.style');
+  font-weight: v-bind('font_subtype.weight');
+  font-size: v-bind(font_size);
+  overflow-y: auto;
+  max-height: calc(100vh - v-bind(outer_size));
+}
+
+.log-list::-webkit-scrollbar {
+  display: none;
+  /* for Chrome, Safari and Opera */
+}
+
+.final-text {
+  color: v-bind(text_color);
+}
+
+.interim-text {
+  color: v-bind(interim_color);
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+
+.fade-out {
+  animation: fadeOut ease v-bind(fade_time);
+  -webkit-animation: fadeOut ease v-bind(fade_time);
+  -moz-animation: fadeOut ease v-bind(fade_time);
+  -o-animation: fadeOut ease v-bind(fade_time);
+  -ms-animation: fadeOut ease v-bind(fade_time);
+  animation-fill-mode: forwards;
+}
+
+@keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+
+@-moz-keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+
+@-webkit-keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+
+@-o-keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+
+@-ms-keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+</style>
