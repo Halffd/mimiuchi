987e546876e37d05364ec23891104aae47ad88bc
Halffd <guhalfeld@hotmail.com>
2024-07-03 20:41:04 -0300

    fix some words not working

--- src/pages/RubyText.vue
+++ src/pages/RubyText.vue
@@ -9,3 +9,38 @@
     <span v-else>{{ safeBaseText }}</span>
   </span>
 </template>
+
+<script>
+export default {
+  name: 'RubyText',
+  props: {
+    baseText: {
+      type: String,
+      required: true
+    },
+    rubyText: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  computed: {
+    hasRuby() {
+      return !!this.rubyText;
+    },
+    baseTextWords() {
+      return this.baseText.split('|').filter(Boolean);
+    },
+    rubyTextWords() {
+      return this.rubyText.split('|').filter(Boolean);
+    },
+    safeBaseText() {
+      return this.baseText.replace(/\|/g, '');
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add any desired styles here */
+</style>
